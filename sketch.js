0834d2d55869fcb445534eb52e86a22ef30179e8
Rohan <rohanvasa58@gmail.com>
2021-10-28 18:55:07 +0530

    added Varible

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var varble;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,11 +18,12 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 300, 180);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    variable = new Box(100,200,200,200);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
